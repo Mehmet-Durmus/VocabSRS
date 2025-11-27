e7423c86ade2a3cd61f6f58c9fd4aeab5e344e85
MehmetDrms <mehmet.drms@outlook.com>
2025-11-27 22:04:55 +0300

    IGenericRepository Implementation

--- VocabSRS.DAL/Repositories/GenericRepository.cs
+++ VocabSRS.DAL/Repositories/GenericRepository.cs
@@ -1,45 +1,65 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VocabSRS.Core.Entities;
 using VocabSRS.Core.Interfaces;
+using VocabSRS.DAL.Context;
 
 namespace VocabSRS.DAL.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
-        
+        private readonly SqlServerContext _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(SqlServerContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
 
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            _dbSet.Add(entity);
+            _context.SaveChanges();
         }
 
         public T Get(int id)
         {
-            throw new NotImplementedException();
+            T value = _dbSet.Find(id);
+            if (value == null)
+                throw new System.NullReferenceException();
+            return value;
         }
 
         public List<T> GetAll()
         {
-            throw new NotImplementedException();
+            List<T> values = _dbSet.ToList();
+            // Custom exception handling
+            return values;
         }
 
         public void HardDelete(int id)
         {
-            throw new NotImplementedException();
+            T value = _dbSet.Find(id);
+            _dbSet.Remove(value);
+            _context.SaveChanges();
         }
 
         public void SoftDelete(int id)
         {
-            throw new NotImplementedException();
+            T value = _dbSet.Find(id);
+            value.IsDeleted = true;
+            _dbSet.Update(value);
+            _context.SaveChanges();
         }
 
         public void Update(T entity)
         {
-            throw new NotImplementedException();
+            _dbSet.Update(entity);
+            _context.SaveChanges();
         }
     }
 }
